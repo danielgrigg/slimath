59f727cdc9d2c12baeb7e7885d39373aeb89bcf4
Daniel Casimir Grigg <mail@danielgrigg.com>
2013-04-16 00:19:47 +1000

    Add another matrix-vec op. Major release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slimath "0.1.5"
+(defproject slimath "0.2.0"
   :description "Simple math library"
   :url "http://sliplanesoftware.com"
   :license {:name "Eclipse Public License"
--- src/slimath/matrix-vec-ops.clj
+++ src/slimath/matrix-vec-ops.clj
@@ -22,6 +22,20 @@
    (+ (* m20 x) (* m21 y) (* m22 z) (* m23 w))
    (+ (* m30 x) (* m31 y) (* m32 z) (* m33 w))])
 
+(defn m34mulv 
+  "Multiply vector by matrix, ignoring bottom row" 
+  [[^double m00 ^double m10 ^double m20 ^double _
+    ^double m01 ^double m11 ^double m21 ^double _
+    ^double m02 ^double m12 ^double m22 ^double _
+    ^double m03 ^double m13 ^double m23 ^double _] 
+   [^double x ^double y ^double z ^double w]]
+
+  [(+ (* m00 x) (* m01 y) (* m02 z)  (* m03 w))
+   (+ (* m10 x) (* m11 y) (* m12 z)  (* m13 w))
+   (+ (* m20 x) (* m21 y) (* m22 z) (* m23 w))
+   w])
+
+
 ;; general macro for dim > 4
 ;(defmacro make-matrix-mul-vec
 ;  [{:keys [start end] :or {start 2}}]
